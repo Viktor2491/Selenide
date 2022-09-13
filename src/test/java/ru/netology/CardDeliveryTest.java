2738202739614ed41b102edb3a985be172166aa7
viktor91 <v.a.taradaiko@gmail.com>
2022-09-13 21:00:57 +0400

    Completing a task

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,12 +1,17 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
+import static java.time.Duration.ofSeconds;
 
 
 public class CardDeliveryTest {
@@ -15,19 +20,24 @@ public class CardDeliveryTest {
     public void setUp() {
         open("http://localhost:9999/");
     }
-     @Test
+
+    @Test
     //валидные значения, отправка запроса
-    public void shouldSubmitRequest(){
+    public void shouldSubmitRequest() {
         $("[placeholder=\"Город\"]").setValue("Москва");
-        $("[placeholder=\"Дата встречи\"]").setValue("16.09.2022");
+        SelenideElement dateField = $("[placeholder=\"Дата встречи\"]");
+        dateField.click();
+        dateField.sendKeys(Keys.CONTROL, "a");
+        dateField.sendKeys(Keys.BACK_SPACE);
+        String dateOfTesting = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        dateField.setValue(dateOfTesting);
         $("[name=\"name\"]").setValue("Иванов Иван");
         $("[name=\"phone\"]").setValue("+79992225533");
         $x("//span[@class =\"checkbox__box\"]").click();
         $x("//span[@class =\"button__text\"]").click();
-        $x("//div [@class=\"notification__icon\"]/..").getText();
-        $x("//* [contains(text(),\"Встреча успешно забронирована на 16.09.2022 \")]").should(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldBe(Condition.visible, ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно забронирована на " + "16.09.2022"));
 
-                
 
- }
+    }
 }
